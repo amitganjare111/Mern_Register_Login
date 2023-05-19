7c9be35dba4ebfc9bd9617aac570933e24748021
Amit Ganjare <amitganjare111@gmail.com>
2023-05-19 23:07:55 +0530

    mern_reg_log

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path = require('path');
 const app= express();
 
 dotenv.config({path: './config.env'});
@@ -11,10 +10,5 @@ app.use(cors());
 app.use(express.json());
 app.use(require('./router/auth'));
 
-//static files
-app.use(express.static(path.join(__dirname, './front/build')));
 
-app.get('*', async (req, res) => {
-    res.sendFile(path.join(__dirname, './front/build/index.html'));
-})
 app.listen(4040);
